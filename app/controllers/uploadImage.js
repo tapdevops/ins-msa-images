0e5c52d49c24bd64c248d6d0861ef5be996c5d5b
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2018-12-04 12:31:30 +0700

    Ferdinand

--- app/controllers/uploadImage.js
+++ app/controllers/uploadImage.js
@@ -1,73 +1,205 @@
+const config = require( '../../config/config.js' ); 	
+const uuid = require( 'uuid' );
+const nJwt = require( 'njwt' );
+const dateAndTimes = require( 'date-and-time' );
+const jwtDecode = require( 'jwt-decode' );
+const randomTextLib = require( '../libraries/randomText' );
 const imageUploadModel = require( '../models/uploadImage.js' );
+const fs = require( 'file-system' );
+const SSHClient = require( 'scp2' );
 
 // Create and Save new Data
-exports.create = ( req, res ) => {
+exports.createDesc = ( req, res ) => {
 
-	if( !req.files ) {
-		return res.status( 400 ).send({
-			status: false,
-			message: 'Invalid file input',
-			data:  {}
+	nJwt.verify( req.token, config.secret_key, config.token_algorithm, ( err, authData ) => {
+		if ( err ) {
+			console.log(err)
+			return res.send( {
+				status: false,
+				message: 'Invalid Token',
+				data: {}
+			} );
+		}
+
+		if ( !req.body.TR_CODE  ) {
+			return res.send( {
+				status: false,
+				message: "Invalid input image description",
+				data: {}
+			} );
+		}
+
+		var trcode = req.body.TR_CODE;
+		var split = trcode.split("-");
+		var auth = jwtDecode( req.token );
+		var randomText = randomTextLib.generate( 5 );
+		var CODE = auth.EMPLOYEE_NIK + 
+			'-' + 
+			dateAndTimes.format( new Date(), 'YYYYMMDD' ) + 
+			'-' + 
+			split[3] + 
+			'-' +
+			split[4] + 
+			'-' + 
+			split[5] + 
+			'-I-' + 
+			randomText;
+		//var folder_target = dateAndTimes.format( new Date(), 'YYYYMMDD' );
+		var imgcode = req.body.IMAGE_CODE;
+		var folder_target = imgcode.split( '-' );
+		var dir = folder_target[1];
+		
+		const set = new imageUploadModel( {
+			IMAGE_CODE: req.body.IMAGE_CODE,
+			TR_CODE: req.body.TR_CODE || "",
+			IMAGE_NAME: req.body.IMAGE_NAME || "",
+			IMAGE_PATH: folder_target[1],
+			STATUS_IMAGE: req.body.STATUS_IMAGE || "",
+			STATUS_SYNC: req.body.STATUS_SYNC || "",
+			SYNC_TIME: new Date(),
+			INSERT_USER: auth.USER_AUTH_CODE,
+			INSERT_TIME: new Date() || "",
+			UPDATE_USER: auth.USER_AUTH_CODE,
+			UPDATE_TIME: new Date() || "",
+			DELETE_USER: ""
 		} );
-	}
 
-	if ( !req.body.IMAGE_CODE || !req.body.TR_CODE  ) {
-		return res.status( 400 ).send( {
-			status: false,
-			message: "Invalid input image description",
-			data: {}
+		set.save()
+		.then( data => {
+			res.send( {
+				status: true,
+				message: 'Success',
+				data: {}
+			} );
+		} ).catch( err => {
+			res.send( {
+				status: false,
+				message: 'Some error occurred while creating data',
+				data: {}
+			} );
 		} );
-	}
 
-	if ( req.files ) {
-		var file = req.files.filename;
-		var filename = file.name;
-		file.mv( './assets/file-upload/' + filename, function( err ) {
+	} );
+};
+
+// Create and Save new Data
+exports.createFile = ( req, res ) => {
+
+	nJwt.verify( req.token, config.secret_key, config.token_algorithm, ( err, authData ) => {
+		if ( err ) {
+			console.log(err)
+			return res.send( {
+				status: false,
+				message: 'Invalid Token',
+				data: {}
+			} );
+		}
+
+		if( !req.files ) {
+			return res.send({
+				status: false,
+				message: 'Invalid file input',
+				data:  {}
+			} );
+		}
+
+		if ( req.files ) {
+
+			var file = req.files.filename;
+			var filename = file.name;
 			
-			if ( err ) {
-				console.log( err );
-				res.json( {
-					message: err
-				} );
-			}
-			else {
-				const set = new imageUploadModel( {
-					IMAGE_CODE: req.body.IMAGE_CODE || "",
-					TR_CODE: req.body.TR_CODE || "",
-					IMAGE_NAME: filename || "",
-					IMAGE_PATH: 'assets/file-upload/' + filename || "",
-					STATUS_IMAGE: req.body.STATUS_IMAGE || "",
-					STATUS_SYNC: req.body.STATUS_SYNC || "",
-					SYNC_TIME: new Date(),
-					INSERT_USER: req.body.INSERT_USER || "",
-					INSERT_TIME: new Date() || "",
-					UPDATE_USER: req.body.INSERT_USER || "",
-					UPDATE_TIME: new Date() || "",
-					DELETE_USER: ""
-				} );
-
-				set.save()
-				.then( data => {
-					res.send( {
-						status: true,
-						message: 'Success',
-						data: {}
-					} );
-				} ).catch( err => {
-					res.status( 500 ).send( {
+			imageUploadModel.findOne( { 
+				IMAGE_NAME : filename,
+				DELETE_USER: "",
+				DELETE_TIME: ""
+			} ).then( data => {
+				if( !data ) {
+					return res.send({
 						status: false,
-						message: 'Some error occurred while creating data',
-						data: {}
+						message: "Gambar tidak ditemukan",
+						data: data,
+					});
+				}
+
+				file.mv( './assets/temp-images/' + filename, function( err ) {
+
+					if ( err ) {
+						return res.send({
+							status: false,
+							message: "Some error occurred while creating data",
+							data: {},
+						});
+					}
+
+					SSHClient.defaults({
+						port: 22,
+						host: '149.129.242.205',
+						username: 'root',
+						password: 'anfieldSG08'
+					});
+
+
+					SSHClient.mkdir( 'development/upload-images/' + data.IMAGE_PATH, function( err ) {
+						if ( err ) {
+							return res.send({
+								status: false,
+								message: "MKDIR Error",
+								data: {},
+							});
+						}
+
+						SSHClient.scp( './assets/temp-images/' + filename, {
+							host: '149.129.242.205',
+							username: 'root',
+							password: 'anfieldSG08',
+							path: '/root/development/upload-images/' + data.IMAGE_PATH + '/'
+						}, function( err ) {
+							if ( err ) {
+								return res.send({
+									status: false,
+									message: "SSH Error",
+									data: {},
+								});
+							}
+
+							fs.unlink( 'assets/temp-images/' + filename, ( err ) => {
+								if ( err ) throw err;
+								console.log( 'assets/temp-images/' + filename );
+							} );
+
+							res.send( {
+								status: true,
+								message: 'Success',
+								data: {}
+							} );
+							
+						});
 					} );
 				} );
-	
-			}
-		} );
-	}
-	else {
-		res.status(500).send({
-			message: "Upload file gagal."
-		});
-	}
-	
+				
+			} ).catch( err => {
+				if( err.kind === 'ObjectId' ) {
+					return res.send({
+						status: false,
+						message: "Error",
+						data: {}
+					});
+				}
+				return res.send({
+					status: false,
+					message: "Error retrieving Data",
+					data: {}
+				} );
+			} );
+			
+		}
+		else {
+			res.send({
+				status: false,
+				message: 'Gagal upload file',
+				data: {}
+			});
+		}
+
+	} );
 };
--- routes/route.js
+++ routes/route.js
@@ -1,27 +1,25 @@
-module.exports = ( app ) => {
-	// Declare Controllers
-	const uploadImage = require( '../app/controllers/uploadImage.js' );
-
-	// ROUTE - UPLOAD
-	app.post( '/upload/image', uploadImage.create );
-}
-/*
-function imageValidation( req, res, next ) { 
+function verifyToken( req, res, next ) {
 	// Get auth header value
-	const file = req.files.filename;
-	const filename = file.name;
-	const mimetype = file.mimetype;
-	const allowedExtensions = [
-		'image/jpeg',
-		'image/png'
-	];
+	const bearerHeader = req.headers['authorization'];
 
-	if ( allowedExtensions.indexOf( mimetype ) >= 0 ) {
+	if ( typeof bearerHeader !== 'undefined' ) {
+		const bearer = bearerHeader.split( ' ' );
+		const bearerToken = bearer[1];
+
+		req.token = bearerToken;
 		next();
 	}
 	else {
 		// Forbidden
 		res.sendStatus( 403 );
 	}
-};
-*/
+}
+
+module.exports = ( app ) => {
+	// Declare Controllers
+	const uploadImage = require( '../app/controllers/uploadImage.js' );
+
+	// ROUTE - UPLOAD
+	app.post( '/image/description', verifyToken, uploadImage.createDesc );
+	app.post( '/image/upload-file', verifyToken, uploadImage.createFile );
+}

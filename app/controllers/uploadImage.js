e3078003482c938181f684f8c58c0dc8c27be2d2
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-14 10:15:22 +0700

    Fer

--- app/controllers/uploadImage.js
+++ app/controllers/uploadImage.js
@@ -312,6 +312,39 @@
 					console.log( new_filename.replace( '.jpeg', '' ) );
 					//http://localhost:3011/finding/F0000005weR
 					console.log('------A');
+
+					imageUploadModel.findOneAndUpdate( { 
+						IMAGE_CODE : req.body.IMAGE_CODE,
+						IMAGE_NAME : req.body.IMAGE_NAME,
+						TR_CODE : req.body.TR_CODE
+					}, {
+						MIME_TYPE: file.mimetype,
+						IMAGE_PATH : directory_target_local,
+						UPDATE_USER: req.body.INSERT_USER || "",
+						UPDATE_TIME: date.convert( req.body.SYNC_TIME, 'YYYYMMDDhhmmss' ),
+					}, { new: true } )
+					.then( data => {
+						if( !data ) {
+							return res.send( {
+								status: false,
+								message: config.error_message.put_404,
+								data: {}
+							} );
+						}
+
+						res.send( {
+							status: true,
+							message: config.error_message.put_200,
+							data: {}
+						} );
+						
+					}).catch( err => {
+						res.send( {
+							status: false,
+							message: config.error_message.put_500,
+							data: {}
+						} );
+					});
 					
 				} );
 				

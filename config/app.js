4f392fa4e0dd30f07290af7f76c4424921878d66
ihsanhusaeri <ihusaeri@gmail.com>
2020-02-25 09:43:47 +0700

    ubah environment ke prod

--- config/app.js
+++ config/app.js
@@ -11,7 +11,7 @@
 		|--------------------------------------------------------------------------
 		*/
 			name: 'Microservice Images',
-			env: 'qa', // prod, qa, dev,
+			env: 'prod', // prod, qa, dev,
 
 			port: {
 				dev: process.env.PORT || 4012,
